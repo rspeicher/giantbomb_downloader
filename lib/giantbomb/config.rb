04e7351a1651ddce27e6faa46e9a18f87f552c42
Robert Speicher <rspeicher@gmail.com>
2019-04-26 10:47:08 -0500

    Move configuration to config/videos.json

--- lib/giantbomb/config.rb
+++ lib/giantbomb/config.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module GiantBomb
   class Config
     def self.config
-      @config ||= YAML.safe_load(
-        File.read(File.expand_path('../../config/videos.yml', __dir__))
-      )
+      @config ||= JSON.parse(File.read(config_file))
+    end
+
+    def self.config_file
+      File.expand_path('../../config/videos.json', __dir__)
     end
   end
 end
